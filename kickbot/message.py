19aedb6f7c4c37e32244457d0e602c55ecc39c3e
Luke <137377630+lukemvc@users.noreply.github.com>
2023-09-02 18:15:10 -0400

    Added ability to set command handler functions. (must be async, and params must be (bot: KickBot, message: KickMessage)

---
+++ kickbot/message.py
@@ -0,0 +1,29 @@
+import json
+
+
+class KickMessage:
+    def __init__(self, raw_data: str):
+        data = json.loads(raw_data)
+        self.data = data
+        self.id: str | None = data.get('id')
+        self.chatroom_id: int | None = data.get('chatroom_id')
+        self.content: str | None = data.get('content')
+        self.type: str | None = data.get('type')
+        self.created_at: str | None = data.get('created_at')
+        self.sender: _Sender | None = _Sender(data.get('sender'))
+
+    def __repr__(self) -> str:
+        return f"KickMessage({self.data})"
+
+
+class _Sender:
+    def __init__(self, raw_sender: dict) -> None:
+        self.raw_sender = raw_sender
+        self.user_id: int | None = raw_sender.get('id')
+        self.username: str | None = raw_sender.get('username')
+        self.slug: str | None = raw_sender.get('slug')
+        self.identity: dict = raw_sender.get('identity')
+        self.badges: list = self.identity.get('badges')
+
+    def __repr__(self) -> str:
+        return f"KickMessage.sender({self.raw_sender})"
