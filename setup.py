aff15f683b067c361dc1743196b281df8b8d1b33
Luke <137377630+lukemvc@users.noreply.github.com>
2023-09-02 22:31:36 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="kickbot",
     version="0.0.1",
@@ -11,25 +14,5 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lukemvc/kickbot",
     packages=find_packages(),
-    install_requires=[
-        'attrs==23.1.0',
-        'certifi==2023.7.22',
-        'charset-normalizer==3.2.0',
-        'exceptiongroup==1.1.3',
-        'h11==0.14.0',
-        'idna==3.4',
-        'outcome==1.2.0',
-        'PySocks==1.7.1',
-        'requests==2.31.0',
-        'selenium==4.12.0',
-        'sniffio==1.3.0',
-        'sortedcontainers==2.4.0',
-        'tls-client==0.2.1',
-        'trio==0.22.2',
-        'trio-websocket==0.10.3',
-        'undetected-chromedriver==3.5.3',
-        'urllib3==2.0.4',
-        'websockets==11.0.3',
-        'wsproto==1.2.0',
-    ],
+    install_requires=requirements,
 )
